541c970edf39b8adea33959b941bd124de892fc6
KawakiParajuli <king.gabu1234@gmail.com>
2023-05-29 22:54:25 -0400

    step 6 and 7

---
+++ blog-service.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const path = require("path");
+
+const postsFile = path.join(__dirname, "data/posts.json");
+const categoriesFile = path.join(__dirname, "data/categories.json");
+
+let posts = [];
+let categories = [];
+
+function initialize() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(postsFile, "utf8", (err, data) => {
+      if (err) {
+        reject("Unable to read posts file");
+      } else {
+        try {
+          posts = JSON.parse(data);
+          fs.readFile(categoriesFile, "utf8", (err, data) => {
+            if (err) {
+              reject("Unable to read categories file");
+            } else {
+              try {
+                categories = JSON.parse(data);
+                resolve();
+              } catch (error) {
+                reject("Error parsing categories file");
+              }
+            }
+          });
+        } catch (error) {
+          reject("Error parsing posts file");
+        }
+      }
+    });
+  });
+}
+
+function getAllPosts() {
+  return new Promise((resolve, reject) => {
+    if (posts.length > 0) {
+      resolve(posts);
+    } else {
+      reject("No results returned");
+    }
+  });
+}
+
+function getPublishedPosts() {
+  return new Promise((resolve, reject) => {
+    const publishedPosts = posts.filter(post => post.published);
+    if (publishedPosts.length > 0) {
+      resolve(publishedPosts);
+    } else {
+      reject("No results returned");
+    }
+  });
+}
+
+function getCategories() {
+  return new Promise((resolve, reject) => {
+    if (categories.length > 0) {
+      resolve(categories);
+    } else {
+      reject("No results returned");
+    }
+  });
+}
+
+module.exports = {
+  initialize,
+  getAllPosts,
+  getPublishedPosts,
+  getCategories
+};
--- server.js
+++ server.js
@@ -1,24 +1,50 @@
+
 var express = require("express");
 var app = express();
 require("dotenv").config();
 var path = require("path");
-
+const blogService = require("./blog-service.js");
 var HTTP_PORT = process.env.PORT || 8080;
-
 // Serve static files from the "public" directory
 app.use(express.static("public"));
-
 // Redirect "/" route to "/about" route
 app.get("/", function(req, res) {
   res.redirect("/about");
 });
-
 // Serve the about.html file from the views folder
 app.get("/about", function(req, res) {
   res.sendFile(path.join(__dirname, "./views/about.html"));
 });
-
-// Start the server and listen on the specified port
-app.listen(HTTP_PORT, function() {
-  console.log("Express http server listening on port " + HTTP_PORT);
-});
+// Initialize blog-service and start the server
+blogService.initialize()
+  .then(() => {
+    // Route to get all published posts
+    app.get("/blog", function(req, res) {
+      blogService.getPublishedPosts()
+        .then(publishedPosts => res.json(publishedPosts))
+        .catch(error => res.status(500).send(error));
+    });
+    // Route to get all posts
+    app.get("/posts", function(req, res) {
+      blogService.getAllPosts()
+        .then(allPosts => res.json(allPosts))
+        .catch(error => res.status(500).send({message : error}));
+    });
+    // Route to get all categories
+    app.get("/categories", function(req, res) {
+      blogService.getCategories()
+        .then(allCategories => res.json(allCategories))
+        .catch(error => res.status(500).send({message : error}));
+    });
+    // Route for unmatched routes
+    app.get("*", function(req, res) {
+      res.status(404).send("Page Not Found");
+    });
+    // Start the server and listen on the specified port
+    app.listen(HTTP_PORT, function() {
+      console.log("Express http server listening on port " + HTTP_PORT);
+    });
+  })
+  .catch(error => {
+    console.error("Failed to initialize blog-service:", error);
+  });
