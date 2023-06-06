3853b2d4890f019f91a05cfc7dec6f47d40df8a8
KawakiParajuli <king.gabu1234@gmail.com>
2023-06-05 22:18:48 -0400

    Part 1 Assignment 3

--- server.js
+++ server.js
@@ -29,29 +29,11 @@ app.get("/", function(req, res) {
 app.get("/about", function(req, res) {
   res.sendFile(path.join(__dirname, "./views/about.html"));
 });
-// Start the server and listen on the specified port
-// app.listen(HTTP_PORT, function() {
-//   console.log("Express http server listening on port " + HTTP_PORT);
-// });
-// // Route to get all published posts
-// app.get("/blog", function(req, res) {
-//   const publishedPosts = blogService.getPublishedPosts();
-//   res.json(publishedPosts);
-// });
-// // Route to get all posts
-// app.get("/posts", function(req, res) {
-//   const allPosts = blogService.getAllPosts();
-//   res.json(allPosts);
-// });
-// // Route to get all categories
-// app.get("/categories", function(req, res) {
-//   const allCategories = blogService.getAllCategories();
-//   res.json(allCategories);
-// });
-// // Route for unmatched routes
-// app.get("*", function(req, res) {
-//   res.status(404).send("Page Not Found");
-// });
+
+//Server the addposts.html file from the views folder.
+app.get("/posts/add", function(req, res){
+  res.sendFile(path.join(__dirname, "./views/addposts.html"));
+});
 
 // Initialize blog-service and start the server
 blogService.initialize()
